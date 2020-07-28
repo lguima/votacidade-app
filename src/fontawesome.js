39b0843428baf253f35f88d7c33e75cbcbb43c18
Lucas Guima <lucasguima.felipe@gmail.com>
2020-07-28 16:27:39 -0300

    Create library for FontAwesome

---
+++ src/fontawesome.js
@@ -0,0 +1,7 @@
+// Import library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// Import icons
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCheck);
