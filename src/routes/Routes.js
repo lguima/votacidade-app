111ed863222b617209d9eb28791b5bbdf0165db0
Lucas Guima <lucasguima.felipe@gmail.com>
2020-08-02 17:03:46 -0300

    Moved the Routes.js
    Implemented the Authenticated route

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,22 +3,29 @@ import { Switch, Route } from 'react-router-dom';
 
 import CityProvider from 'components/CityProvider/CityProvider';
 import LandingPage from 'components/LadingPage';
-import cities from './cities';
-import BaseApp from './BaseApp';
+import cities from '../cities';
+import BaseApp from '../BaseApp';
+
+import AuthenticatedRoute from './AuthenticatedRoute';
+import NotFound from 'pages/NotFound';
 
 const Routes = () => (
   <Switch>
-    <Route path="/" exact>
+    <AuthenticatedRoute exact path="/">
       <LandingPage />
-    </Route>
+    </AuthenticatedRoute>
 
     {cities.map((city) => (
-      <Route path={city.cityPath} key={`${city.cityPath}-route`}>
+      <AuthenticatedRoute path={city.cityPath} key={`${city.cityPath}-route`}>
         <CityProvider city={city}>
           <BaseApp />
         </CityProvider>
-      </Route>
+      </AuthenticatedRoute>
     ))}
+
+    <Route>
+      <NotFound />
+    </Route>
   </Switch>
 );
 
